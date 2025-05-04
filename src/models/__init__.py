cffb06827354fcd07e5179e3e7506c3808ae90cc
lorranmarsaglia35 <lorrancarlos@gmail.com>
2025-05-03 22:12:06 -0300

    Delete src directory

--- src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .admin_user import db, AdminUser
-from .vehicle import Vehicle, VehiclePhoto
-from .settings import Settings
-from .promotion import Promotion
-
-__all__ = ['db', 'AdminUser', 'Vehicle', 'VehiclePhoto', 'Settings', 'Promotion']
